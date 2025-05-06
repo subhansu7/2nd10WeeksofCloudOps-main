148419e80b4a2ab5d8de7eded3edf80d3b25c8df
subhansu7 <sarangisubhansu7@gmail.com>
2025-05-06 12:54:31 +0530

    Update config.js
    changed backedn public IP

--- client/src/pages/config.js
+++ client/src/pages/config.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "http://54.173.114.160:84";
+const API_BASE_URL = "http://54.221.124.33:84";
 // const API_BASE_URL = "http://veera.narni.co.in";
 // export default API_BASE_URL;
 // const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://backend";
